173ad19c2ce42bc5d1ce8c687169dccb03d9e076
Daniel <zankhansql@gmail.com>
2026-06-27 11:22:01 +0530

    frontend 2.1

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,5 +1,7 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 import { Outlet } from 'react-router-dom'
+import LeftSidebar from './components/LeftSidebar';
+import RightSidebar from './components/RightSidebar';
 
 const MainLayout = () => {
   const isMobile = false;
@@ -8,7 +10,7 @@ const MainLayout = () => {
       <ResizablePanelGroup className='flex-1 flex h-full overflow-hidden p-2'>
         {/* Left Sidebar : Playlist */}
         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-          Left Sidebar
+          <LeftSidebar />
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={isMobile ? 100 : 60} maxSize={80}>
@@ -18,7 +20,7 @@ const MainLayout = () => {
         {/* Right Sidebar : Friends Activity */}
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={20} minSize={0} maxSize={30} collapsedSize={0}>
-          Right Sidebar
+          <RightSidebar />
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
