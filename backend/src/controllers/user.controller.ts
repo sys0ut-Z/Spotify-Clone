70c689bedc392f38c226e0c748ad416edbcae8d9
Daniel <zankhansql@gmail.com>
2026-07-08 16:39:37 +0530

    null check issue in user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -29,13 +29,21 @@ const getAllMessages = async (
   next: NextFunction
 ) => {
   const {userId: myId} = getAuth(req);
-  const userId = req.params.userId;
+  const receiver = req.params.userId;
 
+  if(!myId){
+    throw new Error("User is not authenticated, unable to get messages");
+  }
+
+  if(!receiver){
+    throw new Error("Receiver not found, unable to get messages");
+  }
+  
   // console.log("my id : ", myId)
   // console.log("userId : ", userId)
   
   try {
-    const user = await UserModel.findOne({clerkId: userId});
+    const user = await UserModel.findOne({clerkId: receiver});
 
     if(!user){
       throw new Error("User not found, unable to get messages");
@@ -43,8 +51,8 @@ const getAllMessages = async (
 
     const messages = await MessageModel.find({
       $or: [
-        {senderId: myId, receiverId: userId},
-        {senderId: userId, receiverId: myId}
+        {senderId: myId, receiverId: receiver},
+        {senderId: receiver, receiverId: myId}
       ]
     })
     .sort({createdAt: 1}); // latest messages at bottom
