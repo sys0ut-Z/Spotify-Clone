23eef65ef3c2176b2661acf01d5af4d0ac4f97d9
Daniel <zankhansql@gmail.com>
2026-07-08 18:53:23 +0530

    route issue server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -85,7 +85,7 @@ if(process.env.NODE_ENV === "production") {
   // static assets in 'dist' folder that would be served in production
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
-  app.get("*", (req, res) => {
+  app.get("/*anything", (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   })
 }
